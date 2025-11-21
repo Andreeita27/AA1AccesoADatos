2a8972322cc3752cd7bed758932cad38579ef405
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-11-21 12:38:07 +0100

    Corrijo errores de los dtos de la clase Client

--- src/main/java/com/svalero/RosasTattoo/dto/ClientInDto.java
+++ src/main/java/com/svalero/RosasTattoo/dto/ClientInDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,6 +14,6 @@ public class ClientInDto {
     private String clientSurname;
     private String email;
     private String phone;
-    private String birthDate;
+    private LocalDate birthDate;
     private boolean showPhoto;
 }
--- src/main/java/com/svalero/RosasTattoo/dto/ClientOutDto.java
+++ src/main/java/com/svalero/RosasTattoo/dto/ClientOutDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +14,7 @@ public class ClientOutDto {
     private String clientSurname;
     private String email;
     private String phone;
-    private String birthDate;
+    private LocalDate birthDate;
     private boolean showPhoto;
     private int visits;
 }
